14344c382a5d919803eb90ea471fe4a79b02541a
imrePurret <imre.purret@gmail.com>
2011-09-28 23:40:13 +0300

    First layout pages. Some mistakes solved.

--- src/suvaline/pack/OpenActivity.java
+++ src/suvaline/pack/OpenActivity.java
@@ -1,11 +1,13 @@
 package suvaline.pack;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class OpenActivity extends Activity {
@@ -16,14 +18,17 @@ public class OpenActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-    }
-        
-    public void btnSearchClicked(View view) {
-    	setContentView(R.layout.search);
-    }
+        ImageButton next = (ImageButton) findViewById(R.id.btn_Search);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Search.class);
+                startActivityForResult(myIntent, 0);
+            }
 
-
-    
+        });
+    }
+           
+   
     public boolean onCreateOptionsMenu(Menu menu2){ 
 
     	MenuInflater inflater = getMenuInflater(); 
@@ -33,9 +38,12 @@ public class OpenActivity extends Activity {
     	return true;
 
     	}
+    
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.actionbar_item_home:     setContentView(R.layout.main);
+            case R.id.actionbar_item_home:     
+                Intent intent = new Intent(this, OpenActivity.class);
+                this.startActivity(intent);
                                 break;
             case R.id.actionbar_item_create:     Toast.makeText(this, "You pressed the text!", Toast.LENGTH_LONG).show();
                                 break;
