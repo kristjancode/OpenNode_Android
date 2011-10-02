0ec15b03899af90e174e70d9b50bc3dc60bc8d8d
imrePurret <imre.purret@gmail.com>
2011-10-02 14:30:24 +0300

    Login layout + activity.

---
+++ src/suvaline/pack/Login.java
@@ -0,0 +1,56 @@
+package suvaline.pack;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Login extends Activity {
+
+    @Override
+    public void onCreate(final Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.login);        
+        
+        Button login = (Button) findViewById(R.id.btn_login_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), OpenActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+    }
+    public boolean onCreateOptionsMenu(Menu menu2){ 
+
+    	MenuInflater inflater = getMenuInflater(); 
+
+    	inflater.inflate(R.menu.actionbar, menu2); 
+
+    	return true;
+
+    	}
+    
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionbar_item_home:     
+                Intent intent = new Intent(this, OpenActivity.class);
+                this.startActivity(intent);
+                                break;
+            case R.id.actionbar_item_create:     Toast.makeText(this, "You pressed the text!", Toast.LENGTH_LONG).show();
+                                break;
+            case R.id.actionbar_item_search: Toast.makeText(this, "You pressed the icon and text!", Toast.LENGTH_LONG).show();
+                                break;
+        }
+        return true;
+    }
+
+
+}
+
+
