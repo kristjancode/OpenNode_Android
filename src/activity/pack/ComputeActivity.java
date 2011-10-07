69e0075348d80630fce54612eee313a47bf58c1f
imrePurret <imre.purret@gmail.com>
2011-10-07 16:11:38 +0300

    Adding items to android ListView example (ComputeActivity) and some corrections in layouts.

--- src/activity/pack/ComputeActivity.java
+++ src/activity/pack/ComputeActivity.java
@@ -1,20 +1,30 @@
 package activity.pack;
 
 import android.app.Activity;
+import android.app.LauncherActivity.ListItem;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ComputeActivity extends Activity {
-
+	//Example how to add items into list.
+	private ListView computeList;
+	private String listItems[]={"Item1","Item2","Item3","Item4","Item5","Item6","Item7","Item8"};
 	@Override
 	public void onCreate(final Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.compute);
-
+		computeList = (ListView) findViewById(R.id.list_compute);
+		computeList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , listItems));
 	}
 
 	public boolean onCreateOptionsMenu(Menu menu2) {
