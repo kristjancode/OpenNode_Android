dd25363d0e665f61a788232bb3529e35ea80335d
Art Hallap <thelxd@hotmail.com>
2011-10-09 16:00:43 +0300

    new static class for implementing core

---
+++ src/activity/pack/UI_Core.java
@@ -0,0 +1,19 @@
+package activity.pack;
+
+import core.Core;
+import core.config.Config;
+
+public class UI_Core {
+
+	private static Core core;
+	
+	public Core getCore(){
+		return core;
+	}
+	static{
+		Config.SERVER_HOSTNAME = "10.0.2.2";
+		Config.SERVER_PORT = 8080;
+		core = new Core();
+	}
+	
+}
